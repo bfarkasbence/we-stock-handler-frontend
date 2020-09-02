7d75337b32c18d2da90396e67db4b2b27b7336cc
Farkas Bence <b@LAPTOP-TH590FDE>
2020-09-02 13:06:21 +0200

    navbar link changed to a valid path

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -11,7 +11,7 @@ class Navbar extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <a className="navbar-brand" href="#">WestEnd pult</a>
+                <a className="navbar-brand" href="/">WestEnd pult</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse"
                         data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                         aria-expanded="false" aria-label="Toggle navigation">
