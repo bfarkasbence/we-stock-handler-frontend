f3dcab90afa05e58a0205a864455d037534c99f7
Farkas Bence <b@LAPTOP-TH590FDE>
2020-09-17 12:17:37 +0200

    add home page component

---
+++ src/components/pages/home.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from "react;"
+import Axios from "axios";
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function HomePage(props) {
+
+    return(
+    <div className="container" style={{marginLeft: "auto", marginRight: "auto", marginTop: "5%", width: "128 rem"}}>
+        <div className="card">
+            <h4>Home page</h4>
+        </div>
+    </div>
+    )
+}
+export default HomePage;
