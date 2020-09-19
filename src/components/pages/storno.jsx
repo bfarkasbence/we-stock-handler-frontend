43964c52216542ad8266952240f0da125d55b771
Farkas Bence <b@LAPTOP-TH590FDE>
2020-09-19 16:11:31 +0200

    add date picker for getting the selected days sold products

--- src/components/pages/storno.jsx
+++ src/components/pages/storno.jsx
@@ -5,6 +5,24 @@ import { useEffect } from "react";
 import Axios from "axios";
 
 function StornoPage(props) {
+    
+    const [date, setDate] = useState(new Date());
+    
+
+    useEffect(() => {
+        var today = new Date();
+        var dd = String(today.getDate()).padStart(2, '0');
+        var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        var yyyy = today.getFullYear();
+
+        today = yyyy + '-' + mm + '-' + dd;
+        setDate(today);
+    }, [setDate])
+
+
+    const dateOnChange = (event) => {
+        setDate(event.target.value)
+    };
 
 
     return(
@@ -12,6 +30,11 @@ function StornoPage(props) {
             <div className="card">
                 <div className="card-header">
                     <h4>Sztornó</h4>
+                </div>
+                <div className="card-body">
+                    <p>Válassz dátumot:</p>
+                    <p><input type="date" onChange={dateOnChange} value={date}/></p>
+                    <p><button className="btn btn-dark" onClick={() => console.log(date)}>Küldés</button></p>
                 </div>
             </div>
         </div>
