ced27375001c630b3c055e55358ed4f040991fe9
Farkas Bence <b@LAPTOP-TH590FDE>
2020-09-23 15:49:46 +0200

    add a table to show the products

--- src/components/pages/productsToFillUp.jsx
+++ src/components/pages/productsToFillUp.jsx
@@ -7,7 +7,7 @@ function ProductsToFillUp() {
     const [products, setProducts] = useState([]);
 
     useEffect(() => {
-        Axios.get("https://localhost:5001/api/products/fillup")
+        Axios.get("https://localhost:5001/api/product/fillup")
             .then(response => {setProducts(response.data);
             console.log(response.data)});
 
@@ -21,7 +21,26 @@ function ProductsToFillUp() {
                     <h4>Kiszállítandó tételek</h4>
                 </div>
                 <div className="card-body">
-                    <p>itt a listájuk</p>
+                    <table className="table table-striped table-bordered table-hover">
+                        <thead className="thead-dark">
+                            <tr>
+                                <th scope="col">Kód</th>
+                                <th scope="col">Termék neve</th>
+                                <th scope="col">Karton kód</th>
+                                <th scope="col">Szükséges mennyiség</th>
+                                <th scope="col">Kiküldendő mennyiség</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {products.map(product => <tr key={product.id}>
+                                <td>{product.productCode}</td>
+                                <td>{product.name}</td>
+                                <td>{product.cartonCode}</td>
+                                <td>{product.requiredQuantity}</td>
+                                <td>{product.sendQuantity}</td>
+                            </tr>)}
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
