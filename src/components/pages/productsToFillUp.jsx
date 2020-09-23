ae757dc8dbef9a51c68c95ab5a384b5eab898a9f
Farkas Bence <b@LAPTOP-TH590FDE>
2020-09-23 13:00:45 +0200

    add effect to get products to fill up

--- src/components/pages/productsToFillUp.jsx
+++ src/components/pages/productsToFillUp.jsx
@@ -1,7 +1,17 @@
+import Axios from "axios";
 import React from "react";
+import { useState, useEffect } from "react";
 
 function ProductsToFillUp() {
 
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        Axios.get("https://localhost:5001/api/products/fillup")
+            .then(response => {setProducts(response.data);
+            console.log(response.data)});
+
+    }, [setProducts])
 
 
     return (
