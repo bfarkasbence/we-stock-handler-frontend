e8114d10e7a98dc7eea44a9a465485f5a2d26e98
Farkas Bence <b@LAPTOP-TH590FDE>
2020-09-23 12:35:58 +0200

    create a container with one card, where the products will be shown

---
+++ src/components/pages/productsToFillUp.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function ProductsToFillUp() {
+
+
+
+    return (
+        <div className="container" style={{marginLeft: "auto", marginRight: "auto", marginTop: "5%", width: "100%"}}>
+            <div className="card">
+                <div className="card-header">
+                    <h4>Kiszállítandó tételek</h4>
+                </div>
+                <div className="card-body">
+                    <p>itt a listájuk</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProductsToFillUp;
